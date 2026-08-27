327b2611975e9d3adcfc483fbe03939acea0c0b9
EngAliMAhdi <eng.aliosama.mahdi@gmail.com>
2026-08-28 01:02:13 +0300

    Handle POST redirects from Tranzila (payment success/fail)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureAdmin::class,
         ]);
+
+        $middleware->validateCsrfTokens(except: [
+            'payment-success',
+            'payment-fail',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::any('/', function () {
+    $index = public_path('index.html');
+
+    return file_exists($index)
+        ? response()->file($index)
+        : view('welcome');
+});
+
+// Serve the SPA for any non-API path and any HTTP method.
+// Tranzila redirects back with a POST to /payment-success and /payment-fail.
+Route::any('/{any}', function () {
     $index = public_path('index.html');
 
     if (file_exists($index)) {
         return response()->file($index);
     }
 
-    return view('welcome');
-});
-
-Route::fallback(function (Request $request) {
-    if ($request->is('api/*')) {
-        return response()->json(['message' => 'Not Found'], 404);
-    }
-
-    $index = public_path('index.html');
-
-    if ($request->isMethod('GET') && file_exists($index)) {
-        return response()->file($index);
-    }
-
     abort(404);
+})->where('any', '^(?!api/).*');
+
+Route::fallback(function () {
+    return response()->json(['message' => 'Not Found'], 404);
 });
